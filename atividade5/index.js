e5dc17c6b21df64a2f17fadf08b8a827ad2d1b52
Matheus de Oliveira Cavalcante <matheus.ocavalcante@a.ucb.br>
2023-09-27 11:07:14 -0300

    Atividade 5

---
+++ atividade5/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mustacheExpress = require ('mustache-express');
+const app = express();
+
+//app.get('/', function (req, res){
+//    res.send('Olá, mundo !!!');
+
+//});
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/', function (req, res){
+
+    let usuario = {
+        //nome: req.params.n,
+
+        nome: "Matheus de Oliveira Cavalcante",
+        telefone: 123123
+    };
+
+    res.render('index.html',{usuario} );
+
+});
+
+/*
+app.get('/dados', function(req, req){
+    let usuario = {
+        nome: req.query.nome,
+        telefone: req.query.telefone
+    };
+    res.render('dados.html', {usuario} )
+});
+*/
+
+app.post('/dados', function(req, res){
+    let usuario = {
+        nome: req.body.nome,
+        telefone: req.body.telefone
+    };
+    res.render('dados.html', {usuario} )
+});
+
+const PORT = 8080;
+app.listen(PORT, function (){
+    console.log('app rodando na porta ' + PORT);
+});
